416505bf0df97e2e0e13d0b0f928d56a2d2ae90a
Shane Brouwer <shane@findesign.com.au>
2014-09-25 12:51:51 +1000

    Remake Mode_Globals

---
+++ XNAModeGame/XNAModeGame/Mode/Mode_Globals.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XNAMode
+{
+    /// <summary>
+    /// FlxGlobals stores a bunch of constants
+    /// </summary>
+    public class Mode_Globals
+    {
+
+        public static bool CREATE_FROM_OGMO_LEVEL = false;
+
+        /// <summary>
+        /// Number of players for multiplayer.
+        /// </summary>
+        public static int PLAYERS = 0;
+    }
+}
